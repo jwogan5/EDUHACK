510023ef0ee825f7779ec48336eac169a2b2b519
David Orick <dorickweb@gmail.com>
2012-09-20 21:37:27 -0400

    Updating JS for the Lesson page
    added the necessary JS to save the lesson plan and make the ajax call

--- lesson.php
+++ lesson.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" media="screen" type="text/css" href="lesson.css" />   
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Shadows+Into+Light">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	<script src="cookie.js"></script>
 	<script src="lesson.js"></script>
 	<script>
 	
@@ -17,11 +18,10 @@
 	</script>
 </head>
 <body>   
-
 <div class="container">
 
 	<div class="nav">
-		<a href="/search.html" class="searchback"></a>
+		<a href="/eduhack/search.html" class="searchback"></a>
 	</div>	
 	<div class="lesson">
 		<div class="subject">
@@ -42,13 +42,13 @@
 				}			
 			}
 			else{
-				echo '<a href="/search.php" class="gotosearch"></a>';
+				echo '<span class="resourcesbtnlabel">Resources:</span><a href="/search.php" class="gotosearch"></a>';
 			}
 			?>			
 			<span class="purpose">Purpose:</span><input type="text" class="purposetext" />
 			<br class="clear" />			
 			<span class="time">Time:</span>
-			<select>
+			<select class="time">
 				<?
 				for($i=1;$i<=60;$i++){
 					echo '<option value="'.$i.'">'.$i.'</option>';
